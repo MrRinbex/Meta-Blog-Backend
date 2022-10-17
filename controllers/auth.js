8e6c1e1d4581f0b5480fd529afaafcd6b67eec3d
Léo Cheffi <contact@leocheffi.com>
2022-10-17 23:52:32 +0200

    try resolve issue with proxy

--- controllers/auth.js
+++ controllers/auth.js
@@ -51,7 +51,7 @@ export const login = (req, res) => {
     res
       .cookie("access_token", token, {
         httpOnly: true,
-        domain: "https://meta-blog-3-0.netlify.app",
+        maxAge: 1000 * 60 * 60,
       })
       .status(200)
       .json(other);
