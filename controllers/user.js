21f3fbbf8c833059f744ee9c39f9eb332d48a524
MrRinbex <contact@leocheffi.com>
2022-10-23 06:10:19 +0200

    render data after edit profile

--- controllers/user.js
+++ controllers/user.js
@@ -43,12 +43,11 @@ export const updateUser = (req, res) => {
     const values = [req.body.username, req.body.email, hash, req.body.img];
 
     database.query(q, [...values, userId, userInfo.id], (err, data) => {
-      const { password, ...other } = data[0];
       if (err) {
         console.log(err);
         return res.status(500).json(data);
       }
-      return res.json(other, "User has been updated !").status(201);
+      return res.json(data[0], "User has been updated !").status(201);
     });
   });
 };
