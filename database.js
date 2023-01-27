a7208149297bd076589dd7b22c278c58a8ec52e4
MrRinbex <contact@leocheffi.com>
2023-01-27 17:29:18 +0100

    database config

--- database.js
+++ database.js
@@ -2,9 +2,9 @@ import mysql from "mysql2";
 import "./loadEnv.js";
 
 export const database = mysql.createPool({
-  host: process.env.HOST,
-  user: process.env.ROOT,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
   port: process.env.MYSQLPORT,
 });
